70c0bbf9f21b4f8cd5b3bfe80aba53fe7708ab86
nocategory <me@nocategory.dev>
2021-01-10 20:56:18 +0000

    styling exercise

--- App.js
+++ App.js
@@ -4,24 +4,51 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 const App = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={styles.container}>
-        <Text style={styles.textColor}>Hello world!</Text>
+      <Text style={{ fontWeight: 'bold', fontSize: 17, marginVertical: 20 }}>
+        Here are some boxes with different colours:
+      </Text>
+      <View style={[styles.container, styles.violetColor]}>
+        <Text style={styles.text}>violet</Text>
+      </View>
+      <View style={[styles.container, styles.blueColor]}>
+        <Text style={styles.text}>blue</Text>
+      </View>
+      <View style={[styles.container, styles.greenColor]}>
+        <Text style={styles.text}>aquamarine</Text>
+      </View>
+      <View style={[styles.container, styles.redColor]}>
+        <Text style={styles.text}>red</Text>
       </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  textColor: {
+  violetColor: {
+    backgroundColor: '#46237A',
+  },
+  blueColor: {
+    backgroundColor: '#256EFF',
+  },
+  greenColor: {
+    backgroundColor: '#3DDC97',
+  },
+  redColor: {
+    backgroundColor: '#FF495C',
+  },
+  text: {
     color: '#FFF',
-    fontSize: 24,
+    fontSize: 17,
   },
   safeArea: {
     flex: 1,
+    margin: 20,
   },
   container: {
     backgroundColor: '#201f24',
-    flex: 1,
+    margin: 7,
+    padding: 7,
+    borderRadius: 7,
     alignItems: 'center',
     justifyContent: 'center',
   },
